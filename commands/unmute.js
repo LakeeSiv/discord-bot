5589c218f5756778943b8dea9121f30298404c72
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 13:44:46 +0100

    removed uncessary text

--- commands/unmute.js
+++ commands/unmute.js
@@ -5,7 +5,6 @@ module.exports = (client) => {
   command(client, "unmute-basil", (message) => {
     const { member } = message;
     const target = process.env.BAS_ID;
-    const allowed_users = [process.env.LAKEE_ID, process.env.HAR_ID];
     let found = false;
 
     let members = message.channel.members;
