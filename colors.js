91e8465ad6260cf0914c1129108a1ffdd7eebfbd
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 13:41:34 +0100

    updated colors

--- colors.js
+++ colors.js
@@ -1,5 +1,8 @@
 const green = (text) => {
-    return ("```diff\n+" + text + "\n```")
+  return "```diff\n+" + text + "\n```";
 };
-
+const red = (text) => {
+  return "```diff\n-" + text + "\n```";
+};
 exports.green = green;
+exports.red = red;
