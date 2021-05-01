6be1c388f3681a6b79dddb8dd3296c8f7398c4dc
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 12:13:03 +0100

    formatting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-const Discord = require("discord.js")
-const client = new Discord.Client()
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-client.on("ready", ()=> {
-    console.log(token)
-})
+client.on("ready", () => {
+  console.log("Logged in");
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
