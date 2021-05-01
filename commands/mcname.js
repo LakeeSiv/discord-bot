febd1612443b164003af53a7d46477e214d3ce73
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 18:42:42 +0100

    updated commands

--- commands/mcname.js
+++ commands/mcname.js
@@ -1,12 +1,18 @@
 const command = require("../command");
-const { green, red } = require("../colors");
+const usernames = require("../constants/usernames");
+
+const { code } = require("../colors");
 
 module.exports = (client) => {
-  command(client, "username", (message) => {
+  command(client, "mcname", (message) => {
     const { member, content, channel, mentions } = message;
 
-    const syntax = "!mcname <@discordUser>"
-    const split = content.trim().split()
+    const syntax = "!mcname <@discordUser>";
+    const splitContent = content.trim().split(" ");
+    const mentionID = splitContent[1].slice(0, -1).substring(3);
+    const discordName = mentions.users.first().username;
+    const IGN = usernames[mentionID]
 
+    channel.send(code(`@${discordName}'s IGN is ${IGN}`))
   });
 };
