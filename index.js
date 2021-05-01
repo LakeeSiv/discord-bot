ee9e7fa2bfa1821cda674d241d2df830cbc20bd5
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 13:41:57 +0100

    updated index

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const command = require("./command");
 const mute = require("./commands/mute");
+const unmute = require("./commands/unmute");
 
 client.on("ready", () => {
   console.log("Logged in");
 
   mute(client);
+  unmute(client);
 });
 
 client.login(process.env.TOKEN);
