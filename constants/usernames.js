6b197315a6943af906c322cd0119ad9723980419
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 18:13:27 +0100

    mapped id to username

---
+++ constants/usernames.js
@@ -0,0 +1,8 @@
+module.exports= {
+    [process.env.LAKEE_ID] : "lakeeshan",
+    [process.env.HAR_ID] : "ImAlternative",
+    [process.env.BAS_ID] : "Rizal54",
+    [process.env.PHIL_ID] : "_NetflixAndPhil_",
+    [process.env.JOE_ID ]: "Joseph2703",
+    [process.env.DAN_ID ]: "lostmyallowance",
+};
