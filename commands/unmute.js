ce6c3f2c2032f08b3fc3db23def7b42590654b70
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 13:44:06 +0100

    finished unmute

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,26 +1,24 @@
 const command = require("../command");
-const {green} = require("../colors")
+const { green, red } = require("../colors");
 
 module.exports = (client) => {
   command(client, "unmute-basil", (message) => {
     const { member } = message;
-    const target = process.env.LAKEE_ID;
+    const target = process.env.BAS_ID;
     const allowed_users = [process.env.LAKEE_ID, process.env.HAR_ID];
     let found = false;
 
-      let members = message.channel.members;
-      members.forEach((member) => {
-        if (member.id === target) {
-          member.voice.setMute(false);
-          found = true;
-        }
-      });
-      if (!found) {
-        message.channel.send("`Basil is not in the chat`");
-      }else{ 
-        // message.channel.send("```diff\n+ Successfully unmuted basil\n```");
-        message.channel.send(green("Successfully unmuted basil"));    
+    let members = message.channel.members;
+    members.forEach((member) => {
+      if (member.id === target) {
+        member.voice.setMute(false);
+        found = true;
       }
-
+    });
+    if (!found) {
+      message.channel.send(red("Basil is not in the chat"));
+    } else {
+      message.channel.send(green("Successfully unmuted basil"));
+    }
   });
 };
