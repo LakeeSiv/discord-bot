1d5e00b7b7b9aa4d0d21fa8c6624ed0cf854700e
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 18:45:05 +0100

    changes

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,13 @@ const command = require("../command");
 
 const text =
   "```\n\
-!help         : brings up this help message\n\n\
-!github       : returns an embeded link to the github repo\n\n\
-!mute-basil   : this command will server mute basil\n\
+!help           : brings up this help message\n\n\
+!github         : returns an embeded link to the github repo\n\n\
+!mcname <@name> : tag someone on discord with this command to find their IGN\n\
                     permissions: Lakee, Harley\n\n\
-!unmute-basil : unmutes basil from a server mute\n\
+!mute-basil     : this command will server mute basil\n\
+                    permissions: Lakee, Harley\n\n\
+!unmute-basil   : unmutes basil from a server mute\n\
                     permissions: Everyone but basil\n\n\n\n\
 --------------------------------------------------\n\
 MINECRAFT BOT BY LAKEE SIVARAYA\n\
