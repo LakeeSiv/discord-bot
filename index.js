e90442d81154f3119f1598b5d5536eb454043f30
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 12:34:53 +0100

    starting mute command

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const command = require("./command");
 
 client.on("ready", () => {
   console.log("Logged in");
+
+  command(client, "mutebasil", (message) => {
+    message.channel.send(process.env.BAS_MUTE_MESSAGE);
+  });
 });
 
 client.login(process.env.TOKEN);
