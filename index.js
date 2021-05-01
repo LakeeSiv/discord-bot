9d236325434f324bf67ed13b0b993c6a299f7e40
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-01 14:27:55 +0100

    added help command

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const client = new Discord.Client();
 const command = require("./command");
 const mute = require("./commands/mute");
 const unmute = require("./commands/unmute");
+const help = require("./commands/help");
+
 
 client.on("ready", () => {
   console.log("Logged in");
 
   mute(client);
   unmute(client);
+  help(client);
 });
 
 client.login(process.env.TOKEN);
