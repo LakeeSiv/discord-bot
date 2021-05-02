76dbb206a5864f3b056c7ef9fad23c0ac37f61ea
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-02 12:45:25 +0100

    updated help

--- commands/help.js
+++ commands/help.js
@@ -2,18 +2,28 @@ const command = require("../command");
 
 const text =
   "```\n\
-!help           : brings up this help message\n\n\
-!github         : returns an embeded link to the github repo\n\n\
-!mcname <@name> : tag someone on discord with this command to find their IGN\n\
-                    permissions: Everyone\n\n\
-!mute-basil     : this command will server mute basil\n\
-                    permissions: Lakee, Harley\n\n\
-!unmute-basil   : unmutes basil from a server mute\n\
-                    permissions: Everyone but basil\n\n\n\n\
---------------------------------------------------\n\
+-------------------------------------------------------------------------------------------------\n\
+!help                  : brings up this help message\n\n\
+-------------------------------------------------------------------------------------------------\n\
+!github                : returns an embeded link to the github repo\n\n\
+-------------------------------------------------------------------------------------------------\n\
+!mcname <@name>        : tag someone on discord with this command to find their IGN\n\n\
+-------------------------------------------------------------------------------------------------\n\
+!mute-basil            : this command will server mute basil\n\n\
+                           permissions: Lakee, Harley\n\n\
+-------------------------------------------------------------------------------------------------\n\
+!unmute-basil          : unmutes basil from a server mute\n\n\
+                           permissions: Everyone but basil\n\n\
+-------------------------------------------------------------------------------------------------\n\
+!stats <options> <IGN> : returns stats for the player depending on options\n\n\
+     options available : \n\
+                        user: gives general info about users\n\
+                        bw  : bedwars stats\n\
+\n\n\n\n\
+-------------------------------------------------------------------------------------------------\n\
 MINECRAFT BOT BY LAKEE SIVARAYA\n\
 github: https://github.com/LakeeSiv/discordMCBot\n\
---------------------------------------------------\n\
+-------------------------------------------------------------------------------------------------\n\
 ```\
 ";
 
