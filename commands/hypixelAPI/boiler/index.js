c38a697ad3a1c8ba4ab8cb96bc7b701fc0e14aaf
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-02 12:47:04 +0100

    bedwars

--- commands/hypixelAPI/boiler/index.js
+++ commands/hypixelAPI/boiler/index.js
@@ -1,7 +1,9 @@
 const user = require("./user");
+const bw = require("./bw");
 
 const boilers = {
   user,
+  bw,
 };
 
 module.exports = boilers;
--- commands/hypixelAPI/stats.js
+++ commands/hypixelAPI/stats.js
@@ -1,27 +1,28 @@
-const getData = require("./getData")
+const getData = require("./getData");
 const command = require("../../command");
 const { red } = require("../../colors");
-const {user} = require("./boiler")
+const { user, bw } = require("./boiler");
 
 module.exports = async (client) => {
-    command(client, "stats", async (message) => {
-    
-    const {content, channel} = message
+  command(client, "stats", async (message) => {
+    const { content, channel } = message;
     const splitContent = content.trim().split(" ");
-    const option = splitContent[1]
+    const option = splitContent[1];
 
-    const data = await getData(splitContent[2])
+    const data = await getData(splitContent[2]);
 
-    if (!(data)) {
-      channel.send(red("Error player not found"))
-      return
+    if (!data) {
+      channel.send(red("Error player not found"));
+      return;
     }
-    var text = "Game option not found"
-    switch (option){
-    case "user":
-      channel.send(user(data));
-      break;
+    var text = "Game option not found";
+    switch (option) {
+      case "user":
+        channel.send(user(data));
+        break;
+      case "bw":
+        channel.send(bw(data));
+        break;
     }
   });
 };
-
