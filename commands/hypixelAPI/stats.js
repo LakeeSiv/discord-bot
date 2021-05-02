6cd8d9bb29e80a063553be44d28ba9ce056b674a
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-02 12:07:40 +0100

    updates

--- commands/hypixelAPI/stats.js
+++ commands/hypixelAPI/stats.js
@@ -1,15 +1,26 @@
 const getData = require("./getData")
 const command = require("../../command");
+const { red } = require("../../colors");
+
 
 module.exports = async (client) => {
-    command(client, "userStats", async (message) => {
+    command(client, "stats", async (message) => {
     
     const {content, channel} = message
     const splitContent = content.trim().split(" ");
+    const option = splitContent[1]
 
-    const data = await getData(splitContent[1])
+    const data = await getData(splitContent[2])
 
-    channel.send(data.level);
+    if (!(data)) {
+      channel.send(red("Error player not found"))
+    }
+    var text = "Game option not found"
+    switch (option){
+    case "user":
+      channel.send(data.level, splitContent[1]);
+      break;
+    }
   });
 };
 
