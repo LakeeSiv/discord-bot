5b302cd7f8b5631560aff1a00c6ba066ea61f7b2
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-11 17:54:11 +0100

    finished skywars

--- commands/hypixelAPI/boiler/sw.js
+++ commands/hypixelAPI/boiler/sw.js
@@ -8,12 +8,23 @@ module.exports = (data) => {
 
   const dashes = "-".repeat(nickname.length);
   const text = `\n\
-SKYWARS STATS FOR : ${nickname}\
+SKYWARS STATS FOR : ${nickname}\n\
 --------------------${dashes}\n\
 LEVEL: ${level}\n\
-  WINS/LOSSES: ${wins}/${losses} = ${wins / losses} \n\
+WINS/LOSSES: ${wins}/${losses} = ${Math.round((wins / losses) * 100) / 100} \n\
+KILLS/DEATHS: ${kills}/${deaths} = ${
+    Math.round((kills / deaths) * 100) / 100
+  } \n\n\
+SOLO:\
+    ${json2string(solo)}\n\
 \
+TEAM:\
+    ${json2string(team)}\n\
 \
+RANKED:\
+    ${json2string(ranked)}\n\
+\
+
 `;
   return code(text);
 };
