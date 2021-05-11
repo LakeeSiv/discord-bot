cd49ebd07b6098c7dfc0220f55b946b298ee71b2
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-11 17:19:44 +0100

    improving how it looks on mobile devices

--- commands/help.js
+++ commands/help.js
@@ -2,29 +2,31 @@ const command = require("../command");
 
 const text =
   "```\n\
--------------------------------------------------------------------------------------------------\n\
+\n\
 !help                  : brings up this help message\n\n\
--------------------------------------------------------------------------------------------------\n\
+\n\
 !github                : returns an embeded link to the github repo\n\n\
--------------------------------------------------------------------------------------------------\n\
-!mcname <@name>        : tag someone on discord with this command to find their IGN\n\n\
--------------------------------------------------------------------------------------------------\n\
+\n\
+!mcname <@name>        : tag someone on discord with this command \n\
+                         to find their IGN\n\n\
+\n\
 !mute-basil            : this command will server mute basil\n\n\
                            permissions: Lakee, Harley\n\n\
--------------------------------------------------------------------------------------------------\n\
+\n\
 !unmute-basil          : unmutes basil from a server mute\n\n\
                            permissions: Everyone but basil\n\n\
--------------------------------------------------------------------------------------------------\n\
-!stats <options> <IGN> : returns stats for the player depending on options\n\n\
+\n\
+!stats <options> <IGN> : returns stats for the player depending on\n\
+                         options\n\n\
      options available : \n\
-                        user         : gives general info about users\n\
-                        bw           : few bedwars stats\n\
-                        bw-detailed  : detailed bedwars stats\n\
+                    user         : gives general info about users\n\
+                    bw           : few bedwars stats\n\
+                    bw-detailed  : detailed bedwars stats\n\
 \n\n\
--------------------------------------------------------------------------------------------------\n\
+\n\
 MINECRAFT BOT BY LAKEE SIVARAYA\n\
 github: https://github.com/LakeeSiv/discordMCBot\n\
--------------------------------------------------------------------------------------------------\n\
+\n\
 ```\
 ";
 
