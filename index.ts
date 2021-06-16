2b69a5f137dbcf5f309efc8a353f7bffaaa08e04
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-16 14:56:46 +0100

    index

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { config } from "dotenv";
+config();
+import Discord from "discord.js";
+
+const client: Discord.Client = new Discord.Client();
+// require("dotenv").config();
+// const Discord = require("discord.js");
+// const client = new Discord.Client();
+// const command = require("./command");
+
+// const { mute, unmute, help, github, mcname } = require("./commands");
+// const userStats = require("./commands/hypixelAPI/stats")
+
+// client.on("ready", () => {
+//   console.log("Logged in");
+
+//   mute(client);
+//   unmute(client);
+//   help(client);
+//   github(client);
+//   mcname(client);
+//   userStats(client);
+// });
+
+client.login(process.env.TOKEN);
