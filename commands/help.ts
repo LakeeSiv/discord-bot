2a4ed915878811a2fa15c225c12c56426761b09d
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-16 15:30:22 +0100

    help commands

---
+++ commands/help.ts
@@ -0,0 +1,39 @@
+import { Client } from "discord.js";
+import command from "../helpers/command";
+const text: string =
+  "```\n\
+\n\
+!help                  : brings up this help message\n\n\
+\n\
+!github                : returns an embeded link to the github repo\n\n\
+\n\
+!mute-basil            : this command will server mute basil\n\n\
+                           permissions: Lakee, Harley\n\n\
+\n\
+!unmute-basil          : unmutes basil from a server mute\n\n\
+                           permissions: Everyone but basil\n\n\
+\n\
+!stats <options> <IGN> : returns stats for the player depending on\n\
+                         options\n\n\
+     options available : \n\
+                    user         : gives general info about users\n\
+                    bw           : few bedwars stats\n\
+                    bw-detailed  : detailed bedwars stats\n\
+                    sw           : few skywars stats\n\
+                    sw-detailed  : detailed skywars stats\n\
+\
+\n\n\
+\n\
+MINECRAFT BOT BY LAKEE SIVARAYA\n\
+github: https://github.com/LakeeSiv/discordMCBot\n\
+\n\
+```\
+";
+
+const help = (client: Client) => {
+  command(client, "help", (message) => {
+    message.channel.send(text);
+  });
+};
+
+export default help;
---
+++ commands/index.ts
@@ -0,0 +1,8 @@
+// const mute = require("./mute");
+// const unmute = require("./unmute");
+// const help = require("./help");
+// const github = require("./github");
+// const mcname = require("./mcname");
+
+import help from "./help";
+export { help };
