2c8947fb216d2d09ec07c9c81955e71fe83d500c
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-16 14:57:59 +0100

    removed name func

--- constants/usernames.js
+++
@@ -1,9 +0,0 @@
-module.exports= {
-    [process.env.LAKEE_ID] : "lakeeshan",
-    [process.env.HAR_ID] : "ImAlternative",
-    [process.env.BAS_ID] : "Rizal54",
-    [process.env.PHIL_ID] : "_NetflixAndPhil_",
-    [process.env.JOE_ID ]: "Joseph2703",
-    [process.env.DAN_ID ]: "lostmyallowance",
-    [process.env.TEST_ID ]: "test",
-};
