5b0b92f7c91dc2a79b141a5acaff7e6313f4e8a3
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-21 10:20:03 +0100

    got an array of channels

--- commands/carousel.ts
+++ commands/carousel.ts
@@ -2,6 +2,7 @@ import command from "../helpers/command";
 import { green, red } from "../helpers/colors";
 import {
   Client,
+  GuildChannel,
   GuildChannelManager,
   GuildMember,
   TextChannel,
@@ -12,12 +13,18 @@ const carousel = (client: Client) => {
     const { member } = <{ member: GuildMember }>message;
     //     const target: string = process.env.BAS_ID as string;
     const target = process.env.LAKEE_ID;
-    const Channels: GuildChannelManager = message.guild
+    const channels: (string | null)[] = [];
+
+    const ChannelManager: GuildChannelManager = message.guild
       ?.channels as GuildChannelManager;
-    //@ts-ignore
-    Channels.cache.forEach((a) =>
-      console.log(a, "\n ------------------------")
-    );
+
+    ChannelManager.cache.forEach((c: GuildChannel) => {
+      if (c.type === "voice") {
+        channels.push(c.id);
+      }
+    });
+
+    console.log(channels);
   });
 };
 
