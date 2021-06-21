169c6972eeff0c7d9a19dcd7fd55682ae324f2d7
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-21 10:28:11 +0100

    fix

--- helpers/sleep.ts
+++ helpers/sleep.ts
@@ -1,7 +1,7 @@
-const sleep = (ms: number) {
+const sleep = (ms: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
-} 
+};
 
-export default sleep
+export default sleep;
